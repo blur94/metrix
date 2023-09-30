54988393a60f4ecbd4c5208fdcc9ac7c049e6a26
Gilead Odo <odogilead@gmail.com>
2023-09-30 17:15:24 +0100

    Validation update

--- src/utils/business.ts
+++ src/utils/business.ts
@@ -4,8 +4,6 @@ export const directorsValue = {
   name: "",
   email: "",
   phoneNum: "",
-  //   idType: "",
-  //   idFile: "",
 };
 
 export const corporateBusinessValues = {
@@ -24,19 +22,20 @@ export const CBusinessSchema = z.object({
       (val) => ({ message: `${val} must be 11 digits` })
     ),
 
-  directors: z.array(
-    z.object({
-      name: z.string().nonempty("Director's Name is required"),
-      email: z
-        .string()
-        .email("Invalid Email Format")
-        .nonempty("Director's Email is required"),
-      phoneNum: z.string().regex(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/, {
-        message: "Invalid Phone Number",
-      }),
-    })
-  ).length(2, "There must be at least 2 directors"),
+  directors: z
+    .array(
+      z.object({
+        name: z.string().nonempty("Director's Name is required"),
+        email: z
+          .string()
+          .email("Invalid Email Format")
+          .nonempty("Director's Email is required"),
+        phoneNum: z.string().regex(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/, {
+          message: "Invalid Phone Number",
+        }),
+      })
+    )
+    .length(2, "There must be at least 2 directors"),
 });
-
 
 export type CBusinessProp = z.infer<typeof CBusinessSchema>;
